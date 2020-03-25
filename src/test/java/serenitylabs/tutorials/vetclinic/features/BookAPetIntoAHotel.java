5d3f0222175354487e9889c1e704e539300a7409
John Ferguson Smart <john.smart@wakaleo.com>
2020-03-25 09:01:59 +0000

    Updated solutions

--- src/test/java/serenitylabs/tutorials/vetclinic/features/BookAPetIntoAHotel.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/features/BookAPetIntoAHotel.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import serenitylabs.tutorials.vetclinic.features.questions.TheGuests;
 import serenitylabs.tutorials.vetclinic.features.questions.TheRegisteredGuests;
 import serenitylabs.tutorials.vetclinic.features.tasks.CheckIn;
+import serenitylabs.tutorials.vetclinic.features.tasks.CheckOut;
 import serenitylabs.tutorials.vetclinic.model.APetHotel;
 import serenitylabs.tutorials.vetclinic.model.Pet;
 import serenitylabs.tutorials.vetclinic.model.PetHotel;
@@ -37,6 +38,28 @@ public class BookAPetIntoAHotel {
         petra.should(seeThat(TheRegisteredGuests.in(petHotel), hasItem(ginger)));
 
     }
+
+    @Test
+    public void petra_checks_her_cat_out_of_the_hotel() {
+
+        // GIVEN
+
+        Actor petra = Actor.named("Petra the pet owner");
+        Pet ginger = Pet.cat().named("Ginger");
+        PetHotel petHotel = PetHotel.called("Pet Hotel California");
+
+        petra.wasAbleTo(CheckIn.aPet(ginger).into(petHotel));
+
+        // WHEN
+        petra.attemptsTo(
+                CheckOut.aPet(ginger).from(petHotel)
+        );
+
+        // THEN
+        petra.should(seeThat(TheRegisteredGuests.in(petHotel), not(hasItem(ginger))));
+
+    }
+
 
     @Test
     public void petra_books_her_pet_cat_into_the_hotel_that_is_already_full() {
